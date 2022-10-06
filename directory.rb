0b22ae14f350661f66452a525d0ab4dce4443f7d
Henry Boylan <boylanh3nry@gmail.com>
2022-10-06 17:33:43 +0000

    refactored register

--- directory.rb
+++ directory.rb
@@ -1,22 +1,25 @@
+student_count = 10
+
 puts "The Members of Sky High"
-puts "--Faculty--"                      #First are the teachers
-puts "Principal Powers"                 #and the all important
-puts "Coach Boomer"                     #transport officer for the
-puts "Professor Medulla"                #school
+#First are the teachers and the all important transport officer for the school!
+puts "--Faculty--"                      
+puts "Principal Powers"
+puts "Coach Boomer"
+puts "Professor Medulla"
 puts "All-American Boy"
 puts "Ron Wilson, Bus Driver"
 puts "Nurse Spex"
+#Next come the students or, should I say, the new generation of heroes that Sky High will produce.
 puts "--Students--"
-puts "Will Stronghold"                  #Next come the students
-puts "Layla Williams"                   #or, should I say, the
-puts "Gwen Grayson"                     #new generation of heroes
-puts "Warren Peace"                     #that Sky High will produce
+puts "Will Stronghold"                  
+puts "Layla Williams"
+puts "Gwen Grayson"
+puts "Warren Peace"
 puts "Ethan Bank"
 puts "Magenta Lewis"
 puts "Zach Braun"
 puts "Penny Lent"
 puts "Lash"
 puts "Speed"
-print "Overall, we have "                #Lastly, we must take a
-print 10                                 #register of our students!
-puts " great students!"
+#Lastly, we must take a register of our students!
+print "Overall, we have #{student_count} great students"
