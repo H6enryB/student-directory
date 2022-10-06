ed10599b6b599d2541c5222e43655835a815938a
Henry Boylan <boylanh3nry@gmail.com>
2022-10-06 17:02:55 +0000

    Comments added to Directory

--- directory.rb
+++ directory.rb
@@ -1,22 +1,22 @@
 puts "The Members of Sky High"
-puts "--Faculty--"
-puts "Principal Powers"
-puts "Coach Boomer"
-puts "Professor Medulla"
+puts "--Faculty--"                      #First are the teachers
+puts "Principal Powers"                 #and the all important
+puts "Coach Boomer"                     #transport officer for the
+puts "Professor Medulla"                #school
 puts "All-American Boy"
 puts "Ron Wilson, Bus Driver"
 puts "Nurse Spex"
 puts "--Students--"
-puts "Will Stronghold"
-puts "Layla Williams"
-puts "Gwen Grayson"
-puts "Warren Peace"
+puts "Will Stronghold"                  #Next come the students
+puts "Layla Williams"                   #or, should I say, the
+puts "Gwen Grayson"                     #new generation of heroes
+puts "Warren Peace"                     #that Sky High will produce
 puts "Ethan Bank"
 puts "Magenta Lewis"
 puts "Zach Braun"
 puts "Penny Lent"
 puts "Lash"
 puts "Speed"
-print "Overall, we have "
-print 10
+print "Overall, we have "                #Lastly, we must take a
+print 10                                 #register of our students!
 puts " great students!"
